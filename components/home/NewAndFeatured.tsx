fba71e73726f8822106c3d2aa68ea9896ca67334
Xavi-Arnau <chavier28@gmail.com>
2024-06-19 17:44:25 +0200

    products fetch on the main page

--- components/home/NewAndFeatured.tsx
+++ components/home/NewAndFeatured.tsx
@@ -1,6 +1,20 @@
 import NewAndFeaturedProduct from "./NewAndFeaturedProduct";
+import { Product } from "../types/Product";
 
-const NewAndFeatured = () => {
+const NewAndFeatured = async () => {
+  async function fetchProducts() {
+    //should use one of this solutions to be able to use partial urls here https://stackoverflow.com/questions/76995053/how-to-use-relative-path-fetch-in-nextjs-app-router
+    const response = await fetch(
+      "https://boardgames-shop.netlify.app/api/products"
+    );
+
+    const products = await response.json();
+
+    await new Promise((resolve) => setTimeout(resolve, 1000)); //wait 1 second
+
+    return products;
+  }
+  const products = await fetchProducts();
   return (
     <div className="w-full">
       <div className="w-10/12 mx-auto">
@@ -9,18 +23,9 @@ const NewAndFeatured = () => {
         </h2>
 
         <div className="flex flex-col md:flex-row flex-wrap justify-between">
-          <NewAndFeaturedProduct />
-          <NewAndFeaturedProduct />
-          <NewAndFeaturedProduct />
-          <NewAndFeaturedProduct />
-          <NewAndFeaturedProduct />
-          <NewAndFeaturedProduct />
-          <NewAndFeaturedProduct />
-          <NewAndFeaturedProduct />
-          <NewAndFeaturedProduct />
-          <NewAndFeaturedProduct />
-          <NewAndFeaturedProduct />
-          <NewAndFeaturedProduct />
+          {products.map((product: Product) => (
+            <NewAndFeaturedProduct product={product} />
+          ))}
         </div>
       </div>
     </div>
--- components/home/NewAndFeaturedProduct.tsx
+++ components/home/NewAndFeaturedProduct.tsx
@@ -1,18 +1,17 @@
 import { Button } from "../ui/button";
 import { Eye } from "lucide-react";
+import { Product } from "../types/Product";
 
-const NewAndFeaturedProduct = () => {
+interface NewAndFeaturedProductProps {
+  product: Product;
+}
+
+const NewAndFeaturedProduct = ({ product }: NewAndFeaturedProductProps) => {
   return (
     <div className="w-full md:w-1/6 p-4">
       <div className="border-2 rounded-lg flex flex-col items-center group h-[380px] p-4 hover:scale-110 shadow-lg">
-        <img
-          src="/newandfeatured/vida-salvaje-serengeti.jpg"
-          alt=""
-          className="object-fil w-40"
-        />
-        <div className="font-bold text-sm mt-4 h-16">
-          Vida Salvaje: Serengeti
-        </div>
+        <img src={product.picture} alt="" className="object-fil w-40" />
+        <div className="font-bold text-sm mt-4 h-16">{product.name}</div>
         <div className="flex flex-row p-4 justify-between items-center">
           <div className="font-bold text-lg">8,00 €</div>
           <div className="text-sm line-through">19,95 €</div>
