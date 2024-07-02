b2b9a75078de43f56a71b761b4b0b46796949709
Xavi-Arnau <chavier28@gmail.com>
2024-07-02 19:14:54 +0200

    suspense component fix

--- app/(results)/results/page.tsx
+++ app/(results)/results/page.tsx
@@ -1,41 +1,10 @@
-"use client";
-import ResultsProduct from "@/components/results/ResultsProduct";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
-import { Product } from "@/components/types/product";
 import { Suspense } from "react";
+import ResultsList from "@/components/results/ResultsList";
 
 const ResultsPage = () => {
-  const [products, setProducts] = useState([]);
-  const searchParams = useSearchParams();
-  const search = searchParams.get("search");
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(`/api/products/search?query=${search}`);
-      const data = await response.json();
-
-      setProducts(data);
-    };
-
-    fetchData();
-  }, []);
-
   return (
     <Suspense>
-      <div>
-        <div className="w-8/12 mx-auto min-h-[500px]">
-          <h1 className="w-fit p-4 border-2 rounded-lg shadow-lg">
-            Results for: {search}
-          </h1>
-          <div className="flex flex-col md:flex-row flex-wrap justify-between">
-            {products &&
-              products.map((product: Product) => (
-                <ResultsProduct key={product.name} product={product} />
-              ))}
-          </div>
-        </div>
-      </div>
+      <ResultsList />
     </Suspense>
   );
 };
